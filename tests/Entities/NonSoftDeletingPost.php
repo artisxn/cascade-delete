9c8a71443f8e03e4126b91a88746d3b49b3a92dd
Codica Studio <CodicaStudioDev@gmail.com>
2020-08-18 00:39:16 -0500

    Initial commit.

---
+++ tests/Entities/NonSoftDeletingPost.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Entities;
+
+use codicastudio\Database\Support\CascadeSoftDeletes;
+use Illuminate\Database\Eloquent\Model;
+
+class NonSoftDeletingPost extends Model
+{
+    use CascadeSoftDeletes;
+
+    protected $table = 'posts';
+
+    protected $cascadeDeletes = ['comments'];
+
+    protected $fillable = ['title', 'body'];
+
+    public function comments()
+    {
+        return $this->hasMany('Tests\Entities\Comment', 'post_id');
+    }
+}
